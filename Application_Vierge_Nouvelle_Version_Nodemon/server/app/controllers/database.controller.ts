0386aaa5a7638ffa50db07a0bae8e488ecaea9a4
Schleggers <76535747+Schleggers@users.noreply.github.com>
2023-11-18 15:50:23 -0500

    ca marche

--- Application_Vierge_Nouvelle_Version_Nodemon/server/app/controllers/database.controller.ts
+++ Application_Vierge_Nouvelle_Version_Nodemon/server/app/controllers/database.controller.ts
@@ -1,17 +1,33 @@
-import { Router } from "express";
-import { inject, injectable } from "inversify";
-import { DatabaseService } from "../services/database.service";
-import Types from "../types";
+  import { Router } from "express";
+  import { inject, injectable } from "inversify";
+  import { DatabaseService } from "../services/database.service";
+  import Types from "../types";
 
-@injectable()
-export class DatabaseController {
-  public constructor(
-    // @ts-ignore -- À ENLEVER LORSQUE L'IMPLÉMENTATION EST TERMINÉE
-    @inject(Types.DatabaseService) private readonly databaseService: DatabaseService
-  ) {}
+  @injectable()
+  export class DatabaseController {
+    router: Router = Router();
+    public constructor(
+      // @ts-ignore -- À ENLEVER LORSQUE L'IMPLÉMENTATION EST TERMINÉE
+      @inject(Types.DatabaseService) private readonly databaseService: DatabaseService
+    ) {
+      this.handleGet();
+    }
 
-  public get router(): Router {
-    const router: Router = Router();
-    return router;
-  }
+
+    // public get router(): Router {
+    //   const router: Router = Router();
+    //   return router;
+    // } 
+
+    async handleGet() {
+      this.router.get('/patients', async (req, res) => {
+        try {
+          const result = await this.databaseService.query('SELECT * FROM patients');
+          res.json(result.rows);
+        } catch (error) {
+          res.status(500).json({ error: error.message });
+        }  
+      });
+
+    } 
 }
--- Application_Vierge_Nouvelle_Version_Nodemon/server/app/services/database.service.ts
+++ Application_Vierge_Nouvelle_Version_Nodemon/server/app/services/database.service.ts
@@ -5,13 +5,17 @@ import "reflect-metadata";
 @injectable()
 export class DatabaseService {
   public connectionConfig: pg.ConnectionConfig = {
-    user: "postgres",
-    database: "TP4",
-    password: "root",
+    user: "td5_user",
+    database: "hopital",
+    password: "td5",
     port: 5432,          // Attention ! Peut aussi être 5433 pour certains utilisateurs
-    host: "127.0.0.1",
+    host: "localhost",
     keepAlive: true
   };
 
   public pool: pg.Pool = new pg.Pool(this.connectionConfig);
+
+  public async query(text: string, params?: any[]): Promise<pg.QueryResult> {
+    return this.pool.query(text, params);
+  }
 }
