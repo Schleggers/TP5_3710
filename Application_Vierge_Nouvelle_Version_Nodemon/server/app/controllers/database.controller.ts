744ec6f92357520bddbd1ca360fb9a289925e337
Schleggers <76535747+Schleggers@users.noreply.github.com>
2023-11-19 22:29:27 -0500

    la structure est clean at least mais je peux pas post et patch

--- Application_Vierge_Nouvelle_Version_Nodemon/server/app/controllers/database.controller.ts
+++ Application_Vierge_Nouvelle_Version_Nodemon/server/app/controllers/database.controller.ts
@@ -1,112 +1,59 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { inject, injectable } from "inversify";
 import { DatabaseService } from "../services/database.service";
 import Types from "../types";
 
 @injectable()
 export class DatabaseController {
-  router: Router = Router();
   public constructor(
     // @ts-ignore -- À ENLEVER LORSQUE L'IMPLÉMENTATION EST TERMINÉE
     @inject(Types.DatabaseService) private readonly databaseService: DatabaseService
-  ) {
-    this.handleGetMedecins();
-    this.handleDeleteMedecins();
-    this.handlePostMedecins();
-    this.handlePatchMedecins();
-  }
+  ) { } 
+  public get router(): Router {
+    const router: Router = Router();
   
-  async handleGetMedecins() {
-    this.router.get('/medecins', async (req, res) => {
+    router.get('/medecins', async (req: Request, res: Response) => {
       try {
-        const result = await this.databaseService.query(
-            `SELECT M.idMedecin, M.prenom, M.nom, M.specialite, M.anneesExperience, S.nomService 
-            FROM Medecins M
-            JOIN Services S ON M.idService = S.idService`
-        );
-        res.json(result.rows);
+        const results = await this.databaseService.getMedecins();
+        res.json(results.rows);
       } catch (error) {
         res.status(500).json({ error: error.message });
       }  
     });
-  }
    
-  async handlePostMedecins() {
-    this.router.post('/medecins', async (req, res) => {
+
+    router.post('/medecins', async (req: Request, res: Response) => {
       try {
         console.log("post");
-        
-        const { idmedecin, prenom, nom, specialite, anneesExperience, idService } = req.body;
-        const result = await this.databaseService.query(
-          "INSERT INTO Medecins (idmedecin, prenom, nom, specialite, anneesExperience, idService) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-          [idmedecin, prenom, nom, specialite, anneesExperience, idService]
-        );
-        res.json(result.rows[0]);
+        console.log(req.body);
+        const results = await this.databaseService.postMedecins(req.body.idMedecin, req.body.prenom, req.body.nom, req.body.specialite, req.body.anneesExperience, req.body.idService);
+        res.json(results.rows[0]);
       } catch (error) {
         res.status(500).json({ error: error.message });
       }  
     });
-  }
+
    
-  async handlePatchMedecins() {
-    this.router.patch('/medecins/:id', async (req, res) => {
+    router.patch('/medecins/:id', async (req: Request, res: Response) => {
       try {
-        const { prenom, nom, specialite, anneesExperience, idService } = req.body;
-        const idMedecin = req.params.id;
-        const result = await this.databaseService.query(
-          "UPDATE Medecins SET prenom = $1, nom = $2, specialite = $3, anneesExperience = $4, idService = $5 WHERE idMedecin = $6 RETURNING *",
-          [prenom, nom, specialite, anneesExperience, idService, idMedecin]
-        );
-        res.json(result.rows[0]);
+        console.log(req.body);
+        const results = await this.databaseService.patchMedecins(parseInt(req.params.id), req.body.prenom, req.body.nom, req.body.specialite, req.body.anneesExperience, req.body.idService);
+        res.json(results.rows[0]);
       } catch (error) {
         res.status(500).json({ error: error.message });
       }  
     });
-  }
+
     
-  async handleDeleteMedecins() {
-    this.router.delete('/medecins/:id', async (req, res) => {
-      // try {
-      //   const idMedecin = req.params.id;
-      //   const result = await this.databaseService.query(
-      //     "DELETE FROM Medecins WHERE idMedecin = $1 RETURNING *",
-      //     [idMedecin]
-      //   );
-      //   res.json(result.rows[0]);
-      // } catch (error) {
-      //   res.status(500).json({ error: error.message });
-      // }  
-
+    router.delete('/medecins/:id', async (req: Request, res: Response) => {
       try {
-        const idMedecin = req.params.id;
-        await this.databaseService.query('BEGIN');
-        await this.databaseService.query(
-          "DELETE FROM Rendezvous WHERE idMedecin = $1",
-          [idMedecin]
-        );
-        const result = await this.databaseService.query(
-          "DELETE FROM Medecins WHERE idMedecin = $1 RETURNING *",
-          [idMedecin]
-        );
-        await this.databaseService.query('COMMIT');
-        res.json(result.rows[0]);
+        const results = await this.databaseService.deleteMedecins(parseInt(req.params.id));
+        res.json(results.rows[0]);
       } catch (error) {
-        await this.databaseService.query('ROLLBACK');
         res.status(500).json({ error: error.message });
       }  
     });
+  
+    return router;
   }
-
-  // async handleGetServices() {
-  //   this.router.get('/services', async (req, res) => {
-  //     try {
-  //       const result = await this.databaseService.query(
-  //         "SELECT nomService FROM Services"
-  //       );
-  //       res.json(result.rows);
-  //     } catch (error) {
-  //       res.status(500).json({ error: error.message });
-  //     }  
-  //   });
-  // }
 }
--- Application_Vierge_Nouvelle_Version_Nodemon/server/app/services/database.service.ts
+++ Application_Vierge_Nouvelle_Version_Nodemon/server/app/services/database.service.ts
@@ -15,7 +15,53 @@ export class DatabaseService {
 
   public pool: pg.Pool = new pg.Pool(this.connectionConfig);
 
-  public async query(text: string, params?: any[]): Promise<pg.QueryResult> {
-    return this.pool.query(text, params);
+  // public async query(text: string, params?: any[]): Promise<pg.QueryResult> {
+  //   return this.pool.query(text, params);
+  // }
+
+  public async getMedecins(): Promise<pg.QueryResult> {
+    const server = await this.pool.connect();
+    const answer = await server.query(
+      "SELECT M.idMedecin, M.prenom, M.nom, M.specialite, M.anneesExperience, M.idService, S.nomService FROM Medecins M JOIN Services S ON M.idService = S.idService"
+    );
+    server.release();
+    return answer;
   }
+
+  public async postMedecins(idmedecin: number, prenom: string, nom: string, specialite: string, anneesExperience: number, idService: number): Promise<pg.QueryResult> {
+    const server = await this.pool.connect();
+    const answer = await server.query(
+      "INSERT INTO Medecins (idmedecin, prenom, nom, specialite, anneesExperience, idService) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [idmedecin, prenom, nom, specialite, anneesExperience, idService]
+    );
+    server.release();
+    return answer;
+
+  }
+
+  public async patchMedecins(idMedecin: number, prenom: string, nom: string, specialite: string, anneesExperience: number, idService: number): Promise<pg.QueryResult> {
+    const server = await this.pool.connect();
+    const answer = await server.query(
+      "UPDATE Medecins SET prenom = $1, nom = $2, specialite = $3, anneesExperience = $4, idService = $5 WHERE idmedecin = $6 RETURNING *",
+      [prenom, nom, specialite, anneesExperience, idService, idMedecin]
+    );
+    server.release();
+    return answer;
+
+  }
+
+  public async deleteMedecins(idMedecin: number): Promise<pg.QueryResult> {
+    const server = await this.pool.connect();
+    await server.query(
+      "DELETE FROM Rendezvous WHERE idMedecin = $1 RETURNING *",
+      [idMedecin]
+    );
+    const answer = await server.query(
+      "DELETE FROM Medecins WHERE idMedecin = $1 RETURNING *",
+      [idMedecin]
+    );
+    server.release();
+    return answer;
+  }
+    
 }
